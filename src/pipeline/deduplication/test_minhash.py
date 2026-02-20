2af94f7fdcac72ccbdcace33340ab5b7d2fc4107
joausaga <jorgesaldivar@gmail.com>
2026-02-20 16:00:47 -0300

    Add output dir

--- src/pipeline/deduplication/test_minhash.py
+++ src/pipeline/deduplication/test_minhash.py
@@ -6,6 +6,7 @@ from .minhash import execute_minhash_deduplication
 if __name__ == "__main__":
     project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
     data_dir = os.path.join(project_dir, 'data')
-    execute_minhash_deduplication(data_dir)
+    output_dir = os.path.join(project_dir, 'outputs')
+    execute_minhash_deduplication(data_dir, output_dir)
     
     
